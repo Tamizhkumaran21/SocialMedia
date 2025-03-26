baee6ce96eb286bc6893050991e4fe1bf97a7e64
Tamilkumaran0021 <tamilkumaranweboin@gmail.com>
2025-03-26 16:35:40 +0530

    second commit

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
 const jwt  = require('jsonwebtoken');
 
 module.exports = (req, res, next) =>{
-    const token = req.cookies.accesstoken || req.headers("Authorization")?.split(" ")[1];
+    const token = req.cookies.accesstoken || req.headers["Authorization"]?.split(" ")[1];
 
     if(!token){
         return res.status(401).json({message : "Unauthorized"});
